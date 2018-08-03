1772f0ceb7cdd8117a78430afefdade1554e4017
kyori <kyori.dis@gmail.com>
2018-08-03 23:35:10 +0900

    ボタンにスタイルがついてくれると信じている

--- app/javascript/mastodon/features/compose/components/compose_form.js
+++ app/javascript/mastodon/features/compose/components/compose_form.js
@@ -171,7 +171,11 @@ export default class ComposeForm extends ImmutablePureComponent {
       publishText = this.props.privacy !== 'unlisted' ? intl.formatMessage(messages.publishLoud, { publish: intl.formatMessage(messages.publish) }) : intl.formatMessage(messages.publish);
     }
 
-    const buttonStyle = { cursor: "pointer", marginTop: "10px", fontSize: "1.3em", textAlign: "middle" };
+    const buttonStyle = { cursor: "pointer",
+      marginTop: "10px",
+      fontSize: "1.3em",
+      textAlign: "middle",
+      backgroundColor: "#ef5350" };
 
     return (
       <div className='compose-form'>
@@ -220,14 +224,14 @@ export default class ComposeForm extends ImmutablePureComponent {
           <div className='character-counter__wrapper'><CharacterCounter max={4096} text={text} /></div>
         </div>
 
-        <div>
-          <div onClick={this.lookedTootErased} className="flex" >
-            <h1 style={buttonStyle}>トゥ消しを見た！</h1>
-          </div>
-        </div>
-
         <div className='compose-form__publish'>
           <div className='compose-form__publish-button-wrapper'><Button text={publishText} onClick={this.handleSubmit} disabled={disabledButton} block /></div>
+        </div>
+
+        <div>
+          <div onClick={this.lookedTootErased} className="flex" >
+            <button style={buttonStyle} className="button">トゥ消しを見た！</button>
+          </div>
         </div>
       </div>
     );
