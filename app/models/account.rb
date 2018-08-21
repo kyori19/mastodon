ba60643879772e68a2cb5fbad375815f4f582e0c
kyori <kyori.dis@gmail.com>
2018-08-21 17:22:41 +0900

    bio制限解除:表示名64,ノート512,フィールド8

--- app/models/account.rb
+++ app/models/account.rb
@@ -74,9 +74,9 @@ class Account < ApplicationRecord
   validates :username, format: { with: /\A[a-z0-9_]+\z/i }, length: { maximum: 30 }, if: -> { local? && will_save_change_to_username? }
   validates_with UniqueUsernameValidator, if: -> { local? && will_save_change_to_username? }
   validates_with UnreservedUsernameValidator, if: -> { local? && will_save_change_to_username? }
-  validates :display_name, length: { maximum: 30 }, if: -> { local? && will_save_change_to_display_name? }
-  validates :note, length: { maximum: 160 }, if: -> { local? && will_save_change_to_note? }
-  validates :fields, length: { maximum: 4 }, if: -> { local? && will_save_change_to_fields? }
+  validates :display_name, length: { maximum: 64 }, if: -> { local? && will_save_change_to_display_name? }
+  validates :note, length: { maximum: 512 }, if: -> { local? && will_save_change_to_note? }
+  validates :fields, length: { maximum: 8 }, if: -> { local? && will_save_change_to_fields? }
 
   # Timelines
   has_many :stream_entries, inverse_of: :account, dependent: :destroy
